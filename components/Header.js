f7df38a0a48ca2ba9558febcd929b77d1b321032
Adam Andrews <andrewsadam07@gmail.com>
2022-02-07 13:37:43 +0000

    Header Item

---
+++ components/Header.js
@@ -0,0 +1,17 @@
+import {BadgeCheckIcon,HomeIcon} from "@heroicons/react/outline";
+import React from 'react';
+import Image from 'next/image';
+import HeaderItem from "./HeaderItem";
+
+function Header() {
+	return (
+		<header>
+			<div>
+            // <HeaderItem title="Home" Icon={HomeIcon}/>
+            </div>
+			<Image className="object-contain" src="https://links.papareact.com/ua6" width={200} height={100} />
+		</header>
+	);
+}
+
+export default Header;
---
+++ components/HeaderItem.js
@@ -0,0 +1,14 @@
+function HeaderItem({ Icon, title }) {
+	return (
+		<div>
+			<Icon className="h-8 mb-1"/>
+			<p>{title}</p>
+		</div>
+	);
+}
+export default HeaderItem;
+
+{
+	/* <Icon />
+        <p>{title}</p>  */
+}
